49b4bd906fa8492be37a0c36053a0beb9bb601c6
yerinko <yerin_o3o@naver.com>
2021-02-23 23:28:10 +0900

    외부 패키지를 사용할 때 import 문

--- ch02-2/src/person/Person.ts
+++ ch02-2/src/person/Person.ts
@@ -1,4 +1,4 @@
-import * as U from '../utils/makeRandomNumber';
+import {makeRandomNumber} from '../utils/makeRandomNumber';
 import IPerson from "./IPerson";
 
 let MAX_AGE = 100;
@@ -7,10 +7,6 @@ export default class Person implements  IPerson {
     constructor(public name: string, public age: number = makeRandomNumber()) {}
 }
 
-function makeRandomNumber (max : number = MAX_AGE):number {
-    return Math.ceil((Math.random()*max))
-}
-
 export const makePerson = (name: string,
-                    age: number = U.makeRandomNumber()): IPerson => ({name, age});
+                    age: number = makeRandomNumber()): IPerson => ({name, age});
 
