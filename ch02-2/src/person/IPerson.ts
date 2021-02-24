2752a264ed8cef0f69eec1338354ee13ee81f063
yerinko <yerin_o3o@naver.com>
2021-02-24 23:59:22 +0900

    선택 속성 구문

--- ch02-2/src/person/IPerson.ts
+++ ch02-2/src/person/IPerson.ts
@@ -1,12 +1,13 @@
 export default interface IPerson {
-    name: string;
-    age: number
+    name: string; // 필수 속성
+    age: number // 필수 속성
+    etc?: boolean // 선택 속성
 }
 
 // 인터페이스의 조건을 벗어나는 예
-let good: IPerson = { name: 'Jack', age:32}
+let good1: IPerson = { name: 'Jack', age:32};
+let good2: IPerson = { name: 'Jack', age: 32, etc: true}; // etc 속성이 있어서 오류
 
-let bad1: IPerson = { name: 'Jack'} // age 속성이 없으므로 오류
-let bad2: IPerson = { age: 32} // name 속성이 없으므로 오류
-let bad3: IPerson = {} // name과 age 속성이 없으므로 오류
-let bad4: IPerson = { name: 'Jack', age: 32, ect: true} // etc 속성이 있어서 오류
+let bad1: IPerson = { name: 'Jack'}; // age 속성이 없으므로 오류
+let bad2: IPerson = { age: 32}; // name 속성이 없으므로 오류
+let bad3: IPerson = {}; // name과 age 속성이 없으므로 오류
