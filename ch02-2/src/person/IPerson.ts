408fc8b983dce1d1d2398ddb93a7dc49b4fbc50b
yerinko <yerin_o3o@naver.com>
2021-02-24 23:55:52 +0900

    인터페이스 선언문
    IPerson 인터페이스의 목적은 name과 age라는 이름의 속성이 둘 다 있는 객체만 유효하도록 객체의 타입 범위를 좁히는 데 있습니다.

--- ch02-2/src/person/IPerson.ts
+++ ch02-2/src/person/IPerson.ts
@@ -2,3 +2,11 @@ export default interface IPerson {
     name: string;
     age: number
 }
+
+// 인터페이스의 조건을 벗어나는 예
+let good: IPerson = { name: 'Jack', age:32}
+
+let bad1: IPerson = { name: 'Jack'} // age 속성이 없으므로 오류
+let bad2: IPerson = { age: 32} // name 속성이 없으므로 오류
+let bad3: IPerson = {} // name과 age 속성이 없으므로 오류
+let bad4: IPerson = { name: 'Jack', age: 32, ect: true} // etc 속성이 있어서 오류
