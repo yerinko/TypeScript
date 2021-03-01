d34e759c5f02a67bb316ed85bf0043502d42768c
yerinko <yerin_o3o@naver.com>
2021-03-02 00:09:56 +0900

    구조화 (structuring)
    인터페이스나 클래스를 사용해 관련된 정보를 묶어 새로운 타입으로 표현합니다. 코드를 이처럼 구조화하면 코드의 기능 확장이 수월해집니다.

---
+++ ch02-2/src/person/IPerson_IConpany.ts
@@ -0,0 +1,16 @@
+// 구조화가 필요한 코드 예
+let personName = 'Jack';
+let personAge = 32;
+
+let companyName = 'Apple Company, Inc';
+let companyAge = 43;
+
+// 구조화 ( structuring )
+export interface IPerson {
+    name: string;
+    age: number;
+}
+export interface ICompany {
+    name: string;
+    age: number;
+}
