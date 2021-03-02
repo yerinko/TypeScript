da908caf12f2e382323da5d5a29f652d495310ce
yerinko <yerin_o3o@naver.com>
2021-03-03 00:27:29 +0900

    undefined 관련 주의사항
    undefined타입은 타입스크립트의 타입 계층도에서 모든 타입 중 최하위 타입입니다.
    undefined와 null은 ESNext자바스크립트와 타입스ㅡ립트에서 이 두 값은 완전히 같습니다.

---
+++ ch02-2/src/undefined/optional.ts
@@ -0,0 +1,12 @@
+//undefined 관련 주의사항
+interface IAgeable {
+    age?: number
+}
+
+function getAge(o: IAgeable) {
+    return o != undefined && o.age? o.age : 0
+}
+
+console.log(getAge(undefined)); // 0
+console.log(getAge(null)); // 0
+console.log(getAge({age: 32})); //32
