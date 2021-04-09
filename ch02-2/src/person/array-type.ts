0755b9e77423647b5b57b1261311f02f8db1dbfd
yerinko <yerin_o3o@naver.com>
2021-04-09 23:25:40 +0900

    배열의 타입
    타입스크립트에서 배열의 타입은 '아이템 타입[]' 입니다. 예를 들어, 배열의 아이템이 number 타입이면 배열의 타입은 number[]이고, 아이템이 string 타입이면 string[]입니다.

---
+++ ch02-2/src/person/array-type.ts
@@ -0,0 +1,5 @@
+let numArray : number[] = [1,2,3];
+let setArray : string [] = ['Hello', 'world'];
+
+type IPerson = { name: string, age? : number}
+let personArray : IPerson[] = [{name: 'Jack'}, {name: 'Jane', age: "32"}];
--- ch02-2/src/person/isArray.ts
+++ ch02-2/src/person/isArray.ts
@@ -1,3 +1,3 @@
 let a = [1, 2, 3];
 let o = {name: 'Jack', age: 32};
-console.log(Array.isArray(a), Array.isArray(o)); // true fa  lse 
+console.log(Array.isArray(a), Array.isArray(o));gi // true fa  lse
